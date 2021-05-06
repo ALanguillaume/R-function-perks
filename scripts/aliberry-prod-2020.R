453d9ac93ee0ec9592c9e9b5ceb031bf722b610a
ALanguillaume <antoinelanguillaume@gmail.com>
2021-05-06 11:57:12 +0200

    Add title to example plots

--- scripts/aliberry-prod-2020.R
+++ scripts/aliberry-prod-2020.R
@@ -5,10 +5,13 @@ spain <- read.csv("data/Spain_production_2020.csv")
 italy <- read.csv("data/Italy_production_2020.csv")
 
 ggplot(france, aes(x = product, y = production, fill = product))+
-  geom_col()
+  geom_col()+
+  ggtitle(unique(france$country))
 
 ggplot(spain, aes(x = product, y = production, fill = product))+
-  geom_col()
+  geom_col()+
+  ggtitle(unique(spain$country))
 
 ggplot(italy, aes(x = product, y = production, fill = product))+
-  geom_col()
+  geom_col()+
+  ggtitle(unique(italy$country))

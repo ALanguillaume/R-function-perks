f89296e963635ecb0b38bc0a47adb5ac01818c6b
ALanguillaume <antoinelanguillaume@gmail.com>
2021-10-30 14:35:41 +0200

    Add live correction

--- scripts/aliberry-prod-2020.R
+++ scripts/aliberry-prod-2020.R
@@ -6,12 +6,27 @@ italy <- read.csv("data/Italy_production_2020.csv")
 
 ggplot(france, aes(x = product, y = production, fill = product))+
   geom_col()+
-  ggtitle(unique(france$country))
+  ggtitle(unique(france$country))+
+  scale_fill_manual(values = c("darkblue", "darkred"))
 
 ggplot(spain, aes(x = product, y = production, fill = product))+
   geom_col()+
-  ggtitle(unique(spain$country))
+  ggtitle(unique(spain$country))+
+  scale_fill_manual(values = c("darkblue", "darkred"))
 
 ggplot(italy, aes(x = product, y = production, fill = product))+
   geom_col()+
   ggtitle(unique(italy$country))
+
+plot_production <- function(production_df) {
+  ggplot(production_df, aes(x = product, y = production, fill = product))+
+    geom_col()+
+    ggtitle(unique(production_df$country))+
+    scale_fill_manual(values = c("darkblue", "darkred"))
+}
+
+plot_production(italy)
+plot_production(france)
+plot_production(spain)
+# DRY
+
--- scripts/greet-friend.R
+++ scripts/greet-friend.R
@@ -1,8 +1,16 @@
 
-
+## Check list
 # meaningful name
 # function(..) {...}
 # arguments
 # body (recipe)
 # return (explicit/implicit)
 
+## Function to greet a friend
+
+greet_friend <- function(name_friend) {
+  greeting <- paste("Hello", name_friend, "!")
+  return(greeting)
+}
+
+greet_friend("Diane")
